99bee78d31be5168b8a6ec78f34652d5dd50284e
Anton Baglaev <abaglaev@yandex.ru>
2024-03-20 15:57:51 +0300

    add Sem4_DZ1

---
+++ Sem4_DZ1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная.
+
+// void Main(string[] args)
+{
+    string inString = GetDataFromConsole();
+
+    while(true)
+    {
+        if(isExit(inString) || isEvenSumValue(inString))
+        {
+            Console.WriteLine("[STOP]");
+            Environment.Exit(0);
+        }
+        inString = GetDataFromConsole();
+    }
+}
+
+string GetDataFromConsole() 
+{
+    Console.Write("Введите число: ");
+    string? inValue = Console.ReadLine();
+    if(inValue == null) inValue = "";
+    return inValue;
+}
+
+static int ConvertStringToNumber(string number) 
+{
+    try
+    {
+        return Convert.ToInt32(number);
+    } 
+    catch (Exception)
+        {
+        System.Console.WriteLine("Введеное значение не ядляется числом. Попробуйте снова.");
+    }
+    return -1;
+}
+
+static bool isExit(string str) 
+{
+    if(str.Contains('q')) 
+    {
+    return true;
+    }
+    return false;
+}
+
+static bool isEvenSumValue (string str) 
+{
+    int sum = 0;
+    string[] array = str.Split(' ');
+    foreach(string item in array) 
+    {
+        sum += ConvertStringToNumber(item);
+    }
+    return sum % 2 == 0;
+}
