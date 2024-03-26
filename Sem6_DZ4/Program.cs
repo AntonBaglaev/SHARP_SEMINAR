a6dd965e03637c989ea5057fc74f9bd426b0ae7f
Anton Baglaev <abaglaev@yandex.ru>
2024-03-26 12:56:26 +0300

    Sem6_DZ4

---
+++ Sem6_DZ4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string input = "Hello World";
+Console.WriteLine(GetReversWords(input));
+
+static string GetReversWords(string input) 
+{
+    string[] words = input.Split(' ');
+    Array.Reverse(words);
+    string result = "";
+    foreach(string item in words) {
+        result += $"{item}" + " ";
+    }
+    return result.Trim();
+}
