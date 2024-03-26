4cb3bff2201c4e946f11b497f7e4df3500a59e6d
Anton Baglaev <abaglaev@yandex.ru>
2024-03-26 12:38:40 +0300

    add Sem6_DZ1

---
+++ Sem6_DZ1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+char[,] charArray = {{'a','b'},{'c','d'}};
+Console.WriteLine(CreateStringFrom2DArray(charArray));
+
+string CreateStringFrom2DArray(char[,] charArray) 
+{
+    string result = "";
+    foreach(char item in charArray) 
+    {
+        result += $"{item}";
+    }
+    return result;
+}
+        
