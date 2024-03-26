1f1a9fb552220eaa85bd95aa0c576c69a9656b98
Anton Baglaev <abaglaev@yandex.ru>
2024-03-26 12:52:53 +0300

    add Sem6_DZ3

---
+++ Sem6_DZ3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string input = "Поп";
+Console.WriteLine(isPalendrom(input));
+
+bool isPalendrom(string input) 
+{
+    return input.ToLower().SequenceEqual(input.ToLower().Reverse());
+}
